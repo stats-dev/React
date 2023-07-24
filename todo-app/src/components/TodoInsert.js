820159e2deb3b670286845f93cf2c62fdc6e5383
stats-dev <hohun94@naver.com>
2023-07-24 12:36:22 +0900

    useCallback in TodoInsert updates

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 //+ 아이콘 임포트
 import {MdAdd} from 'react-icons/md';
 import '../scss/TodoInsert.scss';
@@ -7,18 +7,19 @@ import '../scss/TodoInsert.scss';
 const TodoInsert = ({addTodos}) => {
   const [text, setText] = useState('');
 
-  const textChange = (e) => {
-    // event가 발생한 타겟의 값을 바꿔버리기.
-    setText(e.target.value);
-  }
-
-  const handleSubmit = (e) => {
+  const textChange = useCallback(
+      (e) => {
+        // event가 발생한 타겟의 값을 바꿔버리기.
+        setText(e.target.value);
+      }, []) //초기 렌더링시 실행?
+    
+  const handleSubmit = useCallback((e) => {
     e.preventDefault();
 
     addTodos(text);
     //새로운 내용이 추가되므로 텍스트는 다시 빈칸.
     setText('');
-  }
+  }, [addTodos, text]); //addTodos 메서드 실행, text를 매개변수로 함께 주어짐.
 
   return (
     <form className='TodoInsert' onSubmit={handleSubmit}>
