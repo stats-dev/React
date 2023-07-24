d27300277f60c380e48bb3088fd31e5b08ec0e34
stats-dev <hohun94@naver.com>
2023-07-24 12:30:52 +0900

    changeCheked method updates

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -4,7 +4,7 @@ import '../scss/TodoList.scss';
 
 
 
-const TodoList = ({todos}) => { 
+const TodoList = ({todos, removeTodos, changeChecked}) => { 
   // todos라는 키로 이걸 받아주겠다.
   return (
     <div className='TodoList'>
@@ -12,7 +12,9 @@ const TodoList = ({todos}) => {
         {/* 단축평가 */}
         {todos && todos.map((todo) => 
           // 객체인 todo 자체도 바로 보낼 수 있다. 굳이 아래처럼 안 나눠도 될 듯 싶다. 이걸 다시 todolistItem에 가서 받아준다.
-          <TodoListItem key={todo.id} todo={todo}></TodoListItem>)}
+          // removeTodos도 함께 보내겠다. TodoListItem으로 전달할 수 있다.
+          // ListItem에도 props로 changeChecked를 보내어 준다.
+          <TodoListItem key={todo.id} todo={todo} removeTodos={removeTodos} changeChecked={changeChecked}></TodoListItem>)}
 
         {/* {todos.map((todo) => (
               <TodoListItem key={todo.id} id={todo.id} text={todo.text} checked={todo.checked}/>
